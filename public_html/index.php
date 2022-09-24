cc8f1518ab5fb391cdb8226ee2ac489f3567cede
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-09-24 14:02:09 +0200

    display onion links aka dir/.../hostname

--- public_html/index.php
+++ public_html/index.php
@@ -23,11 +23,28 @@
   <section id="boxes">
     <div class="container card">
       <h2>
-          Hello There !
+        Hello There !
       </h2>
       <p> I wish you a nice cookie day 🍪</p>
     </div>
   </section>
+
+  <section id="boxes">
+    <div class="container card">
+      <h2><b>Onion Link</b></h2>
+      <?php
+      if (file_exists(__DIR__ . '/../temp/var/lib/tor/hidden_service/hostname')) {
+        $output = file_get_contents(__DIR__ . '/../temp/var/lib/tor/hidden_service/hostname');
+        echo "Congratulation's 🥳🥳<br>Your application is over tor <br>Here is your Onion Link <a href=http://$output>$output</a>";
+      } else {
+        $output = "No domain";
+        echo '<p>Something went wrong<br>Refer to  Github Repository <br><a href="https://github.com/sumithemmadi/tor-hidden-service-on-heroku#readme">https://github.com/sumithemmadi/tor-hidden-service-on-heroku#readme</a></p>';
+        echo '<p>Or raise a issue at <br> <a href="https://github.com/sumithemmadi/tor-hidden-service-on-heroku#readme">https://github.com/sumithemmadi/tor-hidden-service-on-heroku/issues</a></p></br>';
+      }
+      ?>
+    </div>
+  </section>
+  <section id="boxes">
 </body>
 
 </html>
